d6eb3f58dbd2e714503171771bde23ab9b2aaf99
patiljayant <72199264+patiljayant@users.noreply.github.com>
2021-06-02 16:36:04 +0530

    added home component with setup

---
+++ client/src/components/Login.js
@@ -0,0 +1,13 @@
+import '../assets/Login.css';
+
+const Login = () => {
+    return ( 
+        <div className="login">
+            <div className="brand">
+                GetSetWork
+            </div>
+        </div>
+     );
+}
+ 
+export default Login;
