ba4cb86e141fe457f3a503a081e745c6ada644ab
Ankit <ankitrastogi31200@gmail.com>
2021-06-04 22:22:42 +0530

    updated FreelancerProjectCard

--- src/components/FreelancerHome.js
+++ src/components/FreelancerHome.js
@@ -10,7 +10,7 @@ export default function FreelancerHome() {
             <NavBar/>
             <div className='freelancerHome'>
                 <div className="freelancerHome_left">
-                    <div className="freelancer_photo"><img src="https://media-exp3.licdn.com/dms/image/C4E03AQHhhPDR-pWxnw/profile-displayphoto-shrink_800_800/0/1607581026296?e=1628121600&v=beta&t=5xqKd8iFWdyO1-gp_nWd6_t0CUmVkB27sKMVSe_gTaQ" alt="display photo" /></div>
+                    <div className="freelancer_photo"><img src="https://avatars.githubusercontent.com/u/55028717?v=4" alt="display photo" /></div>
                     <div className="freelancer_about">
                         <strong>About</strong>
                         <p>lorem ipsum dolor sit amet, consectetur adipiscing elit, 
