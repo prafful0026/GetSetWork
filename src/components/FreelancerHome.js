9723485930da1afc3d6022b559e49c2c200c8ee4
Ankit <ankitrastogi31200@gmail.com>
2021-06-05 14:05:27 +0530

    added modal in home

--- src/components/FreelancerHome.js
+++ src/components/FreelancerHome.js
@@ -1,13 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../assets/FreelancerHome.css'
 import FreelancerProjectCard from './FreelancerProjectCard'
 import NavBar from './NavBar'
 
 export default function FreelancerHome() {
+    const [yourProjectModal, setYourProjectModal] = useState(false)
+
+    const handleYourProjectClick = () => {
+        if(yourProjectModal){
+            console.log(yourProjectModal);
+            setYourProjectModal(false)
+        }else{
+            console.log(yourProjectModal);
+            setYourProjectModal(true)
+        }
+    }
 
     return (
         <>
             <NavBar/>
+            <div onClick={handleYourProjectClick} className="freelancerHome_yourProjectButton">
+                <i class="fas fa-scroll"></i>
+            </div>
+            {yourProjectModal&&(
+            <div>
+                <div className="modalBackground"></div>
+                <div className="freelancerHome_yourProject">
+                <FreelancerProjectCard none={'none'}/>
+                        <FreelancerProjectCard/>
+                        <FreelancerProjectCard none={'none'}/>
+                        <FreelancerProjectCard none={'none'}/>
+                        <FreelancerProjectCard/>
+                        <FreelancerProjectCard/>
+                        <FreelancerProjectCard/>
+                        <FreelancerProjectCard none={'none'}/>
+                        <FreelancerProjectCard/>
+                </div>
+            </div>
+            )}
             <div className='freelancerHome'>
                 <div className="freelancerHome_left">
                     <div className="freelancer_photo"><img src="https://avatars.githubusercontent.com/u/55028717?v=4" alt="display photo" /></div>
@@ -36,7 +66,6 @@ export default function FreelancerHome() {
                     <FreelancerProjectCard/>
                     <FreelancerProjectCard none={'none'}/>
                     <FreelancerProjectCard/>
-
                 </div>
             </div>
         </>
